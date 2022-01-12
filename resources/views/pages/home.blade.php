c2f6fe45507bd5ac20723e9fec905ad4dbc8ba64
ardamuh <arda.muhamdn@gmail.com>
2022-01-12 09:57:19 +0700

    update

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,10 +6,10 @@
         <div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
           
           <h1 class="my-4 text-5xl font-bold leading-tight">
-            Solusi mudah untuk mengatur keuangan keluarga Anda
+          Way of Financial Success
           </h1>
           <p class="leading-normal text-2xl mb-8">
-            Sub-hero message, not too long and not too short. Make it just right!
+          We help our users to make the right financial planner
           </p>  
         </div>
         <!--Right Col-->
@@ -40,18 +40,21 @@
     <section class="bg-white border-b py-8">
       <div class="container max-w-5xl mx-auto m-8">
         <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
-          Title
+        Some of our wonderful Features
         </h1>
+        <p class="text-gray-600 mb-8 text-center">
+        Here is our wonderful features that we want to highlight for you, so you can understand out app more better.              <br />
+              <br />
+            </p>
         <div class="w-full mb-4">
           <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
         </div>
         <div class="flex flex-wrap">
           <div class="w-5/6 sm:w-1/2 p-6">
             <h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">
-              Lorem ipsum dolor sit amet
-            </h3>
+            Wonderful & Effortless way to control your finances right            </h3>
             <p class="text-gray-600 mb-8">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+            The next generation time-tracking that not only records your financial past, but also helps you plan and predict a profitable future.
               <br />
               <br />
             </p>
@@ -67,10 +70,10 @@
           <div class="w-full sm:w-1/2 p-6 mt-6">
             <div class="align-middle">
               <h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">
-                Lorem ipsum dolor sit amet
+              Keep on eye to Your Expenses with our Limiter
               </h3>
               <p class="text-gray-600 mb-8">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet ligula.
+              Introducing the limiter as the name you can expect it's just a limiter for your expenses.
                 <br />
                 <br />
               </p>
@@ -103,7 +106,7 @@
         </g>
       </g>
     </svg>
-    <section class="container mx-auto text-center py-6 mb-12">
+    <!-- <section class="container mx-auto text-center py-6 mb-12">
       <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-white">
         Call to Action
       </h1>
@@ -116,22 +119,24 @@
       <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
         Action!
       </button>
-    </section>
+    </section> -->
 
     <div class="#f8fafc">
         <div class="lg:grid lg:grid-cols-2">
             <div class="py-24 px-10 lg:px-0 max-w-3xl lg:max-w-md mx-auto">
                 <h2 class="text-4xl tracking-tight font-extrabold text-gray-100">
-                    <span class="block">Ready to dive in?</span>
-                    <span class="block">Start your free trial today.</span>
+                    <span class="block">E-Sangu App </span>
+                    <span class="block">Available Now</span>
                 </h2>
-                <p class="text-gray-300 mt-5">
+                <!-- <p class="text-gray-300 mt-5">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                </p>
+                </p> -->
+                <img class="object-cover object-center" src="{{asset('frontend/download-logo-app-stoter-6.jpg')}}" alt="">
+
                
             </div>
             <div class="lg:relative lg:mt-16">
-                <img class="lg:absolute lg:inset-0 h-60 w-full lg:h-full object-cover object-center lg:rounded-tl-md" src="{{asset('frontend/Mar-Business_11.jpg')}}" alt="">
+                <!-- <img class="lg:absolute lg:inset-0 h-60 w-full lg:h-full object-cover object-center lg:rounded-tl-md" src="{{asset('frontend/Mar-Business_11.jpg')}}" alt=""> -->
                 
               </div>
         </div>
